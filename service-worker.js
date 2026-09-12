3da3dabe3ec419156044a685fb68b0f26f0a4c60
Benedetto-gif <78679378+Benedetto-gif@users.noreply.github.com>
2026-09-12 10:34:42 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "erbario-v5";
+const CACHE_NAME = "GinBuilt-v5";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
